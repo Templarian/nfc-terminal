72eca47c546e3af352711a084d2b17ef87ac4a03
Austin Andrews <admin@templarian.com>
2022-02-28 23:16:08 -0600

    Cleanup

--- src/apis/writeNfc.js
+++ src/apis/writeNfc.js
@@ -1,16 +1,14 @@
-const pn532 = require('pn532');
-const SerialPort = require('serialport');
-const ndef = require('ndef');
 const { writeFileSync } = require('fs');
 const { execSync } = require('child_process');
 
 const { connection, baudRate } = require('./../config');
+const { stringify } = require('querystring');
 
 // ndef Unknown
 const unknown = 5;
 
 function hex(d) {
-    return parseInt(d, 16);
+  return parseInt(d, 16);
 }
 
 // 1 record C5
@@ -18,61 +16,45 @@ function hex(d) {
 // 3 record 95 15 55
 
 function toData(records) {
-    const buffer = [];
-    const count = records.length;
-    records.forEach((record, i) => {
-        if (count === 1) {
-            buffer.push(hex(`c${unknown}`));
-        } else if (i === 0) {
-            buffer.push(hex(`9${unknown}`));
-        } else if (count === i + 1) {
-            buffer.push(hex(`5${unknown}`));
-        } else {
-            buffer.push(hex(`1${unknown}`));
-        }
-        // Spacer
-        buffer.push(0);
-        // Length
-        const total = record.length;
-        const overflow = Math.floor(total / 256);
-        const length = total % 256;
-        if (overflow === 0) {
-            buffer.push(length);
-        } else {
-            buffer.push(overflow);
-            buffer.push(length);
-        }
-        // Write Record
-        for (let i = 0; i < total; i++) {
-            buffer.push(record.charCodeAt(i))
-        }
-    });
-    return buffer.map(code => String.fromCharCode(code)).join('');
+  const buffer = [];
+  const count = records.length;
+  records.forEach((record, i) => {
+    if (count === 1) {
+      buffer.push(hex(`c${unknown}`));
+    } else if (i === 0) {
+      buffer.push(hex(`9${unknown}`));
+    } else if (count === i + 1) {
+      buffer.push(hex(`5${unknown}`));
+    } else {
+      buffer.push(hex(`1${unknown}`));
+    }
+    // Length
+    const total = record.length;
+    const overflow = Math.floor(total / 256);
+    const length = total % 256;
+    buffer.push(overflow);
+    buffer.push(length);
+    // Write Record
+    for (let i = 0; i < total; i++) {
+      buffer.push(record.charCodeAt(i))
+    }
+  });
+  return buffer.map(code => {
+    return String.fromCharCode(code);
+  }).join('');
 }
 
 exports.write = function (records) {
   return new Promise((resolve, reject) => {
     // create file
     writeFileSync('write', toData(records), { encoding: 'binary' });
-    const stdout = execSync('mifare-desfire-write-ndef -i write -y');
-    
-    resolve(stdout);
-    /*var serialPort = new SerialPort(connection, { baudRate });
-    var rfid = new pn532.PN532(serialPort);
-    rfid.on('ready', function () {
-      rfid.scanTag().then(function (tag) {
-        var messages = [
-          ndef.textRecord(value)
-        ];
-        var data = ndef.encodeMessage(messages);
-
-        rfid.writeNdefData(data).then(function (response) {
-          resolve({
-            found: true,
-            response
-          });
-        });
+    try {
+      const stdout = execSync('mifare-desfire-write-ndef -i write -y');
+      resolve(stdout.toString());
+    } catch ({ stderr }) {
+      resolve({
+        error: stderr.toString().replace(/\r/g, '');
       });
-    });*/
+    }
   });
 };
