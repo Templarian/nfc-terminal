a6cd7adfe512de83c2b30eceef0b89d2fb9a727a
Austin Andrews <admin@templarian.com>
2022-03-02 00:07:12 -0600

    Update writeNfc.js

--- src/apis/writeNfc.js
+++ src/apis/writeNfc.js
@@ -1,42 +1,44 @@
 const { writeFileSync } = require('fs');
 const { execSync } = require('child_process');
 
-const { connection, baudRate } = require('./../config');
-const { stringify } = require('querystring');
-
 // ndef Unknown
-const unknown = 5;
+const UNKNOWN = '101';
+const tnf = UNKNOWN;
+// id length (not used)
+const IL = '0';
 
-function hex(d) {
-  return parseInt(d, 16);
-}
-
-// 1 record C5
-// 2 record 95 55
-// 3 record 95 15 55
+// c5 95 55 15
 
 function toData(records) {
   const buffer = [];
   const count = records.length;
   records.forEach((record, i) => {
-    if (count === 1) {
-      buffer.push(hex(`c${unknown}`));
-    } else if (i === 0) {
-      buffer.push(hex(`9${unknown}`));
-    } else if (count === i + 1) {
-      buffer.push(hex(`5${unknown}`));
-    } else {
-      buffer.push(hex(`1${unknown}`));
-    }
+    // Begin
+    const MB = i === 0 ? '1' : '0';
+    // End
+    const ME = count === i + 1 ? '1' : '0';
+    // Fragment
+    const CF = count !== i + 1 ? '1' : '0';
     // Length
     const total = record.length;
     const overflow = Math.floor(total / 256);
     const length = total % 256;
-    if (overflow !== 0) {
+    // Short Record
+    const SR = overflow === 0 ? '1' : '0';
+    // Combined
+    const binary = parseInt(`${MB}${ME}${CF}${SR}${IL}${tnf}`, 2);
+    buffer.push(binary);
+    // Type Length (not used for binary data)
+    buffer.push(0);
+    if (SR === '1') {
+      buffer.push(length);
+    } else {
       buffer.push(0);
+      buffer.push(0);
+      // 256 * Overflow + Length
+      buffer.push(overflow);
+      buffer.push(length);
     }
-    buffer.push(overflow);
-    buffer.push(length);
     // Write Record
     for (let i = 0; i < total; i++) {
       buffer.push(record.charCodeAt(i))
