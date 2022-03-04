34f9fbe0934a9b65975a6e63e1a3702a3358ede1
Austin Andrews <admin@templarian.com>
2022-03-03 22:55:25 -0600

    cleanup

--- src/apis/firmwareNfc.js
+++ src/apis/firmwareNfc.js
@@ -1,17 +1,5 @@
-const pn532 = require('pn532');
-const SerialPort = require('serialport');
-
-const { connection, baudRate } = require('./../config');
-
 exports.firmware = function () {
     return new Promise((resolve, reject) => {
-        var serialPort = new SerialPort(connection, { baudRate });
-        var rfid = new pn532.PN532(serialPort);
-        rfid.on('ready', function() {
-            rfid.getFirmwareVersion().then(function(data) {
-                console.log('firmware: ', data);
-                resolve(data);
-            });
-        });
+        resolve('test');
     });
 };
