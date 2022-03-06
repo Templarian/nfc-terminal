1476bf000e6f25237465310c26ac94a5633d356c
Austin Andrews <admin@templarian.com>
2022-03-06 17:03:42 -0600

    progress

---
+++ src/apis/pollNfc.js
@@ -0,0 +1,1 @@
+// This restarts after every
--- src/apis/readNfc.js
+++ src/apis/readNfc.js
@@ -4,9 +4,45 @@ const { execSync } = require('child_process');
 /**
  * NDef Records -> String[]
  */
-function fromData(arrayBuff) {
-
-    return [];
+function fromData(buffer) {
+    const records = [];
+    let index = 0,
+        length = 0;
+    do {
+        switch(buffer.at(index)) {
+            case '\x95':
+            case '\x15':
+            case '\x55':
+            case '\xd5':
+                // Short Record = 1
+                index++; // 00
+                index++; // Length 0 to 255
+                length = buffer.charCodeAt(index);
+                index++;
+                records.push(buffer.substr(index, length));
+                index += length;
+                break;
+            case '\x85':
+            case '\x05':
+            case '\x45':
+            case '\xc5':
+                // Short Record = 0
+                index++; // 00
+                index++; // 00
+                index++; // Length 0 to 255
+                var v = buffer.charCodeAt(index);
+                index++; // Length 0 to 255
+                length = buffer.charCodeAt(index) + (v * 256);
+                index++;
+                records.push(buffer.substr(index, length));
+                index += length;
+                break;
+            default:
+                // Really only support binary
+                throw 'NDEF Unknown x05 only!';
+        }
+    } while (index !== buffer.length);
+    return records;
 }
 
 exports.read = function () {
@@ -15,7 +51,8 @@ exports.read = function () {
             // Write NFC data to "read" file
             const stdout = execSync('mifare-desfire-read-ndef -o read -y');
             // Read File
-            readFileSync('read', { encoding: 'binary' });
+            const data = readFileSync('read', { encoding: 'binary' });
+            resolve(fromData(data));
         } catch ({ stderr }) {
             const message = stderr.toString().replace(/\r/g, '');
             if (message.match(/mifare-desfire-read-ndef/)) {
@@ -32,5 +69,10 @@ exports.read = function () {
 };
 
 exports.test = function () {
-    
+    const data = readFileSync('src/assets/mock/write', { encoding: 'binary' });
+    const records = fromData(data);
+    if (records.length !== 5) {
+        throw new Error(JSON.stringify(records));
+    }
+
 }
