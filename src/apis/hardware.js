47dca0c704265bd517a1c72460d8e0210d99aebf
Austin Andrews <admin@templarian.com>
2022-03-06 20:42:39 -0600

    Update hardware.js

--- src/apis/hardware.js
+++ src/apis/hardware.js
@@ -2,6 +2,6 @@ const { execSync } = require('child_process');
 
 exports.getTemp = function () {
     const stdout = execSync('vcgencmd measure_temp');
-    console.log(stdout.toString());
-    return parseInt(stdout.toString(), 10);
+    const temp = stdout.toString().replace(/[^0-9\.]+/g, '');
+    return temp;
 };
